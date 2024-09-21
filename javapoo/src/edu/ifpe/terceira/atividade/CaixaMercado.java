2dad9a0e970eae4871b405d503df876f45e5c767
Vagner-Alves <vagneralves997@gmail.com>
2024-09-20 23:28:27 -0300

    atualizando os exercicios

--- javapoo/src/edu/ifpe/terceira/atividade/CaixaMercado.java
+++ javapoo/src/edu/ifpe/terceira/atividade/CaixaMercado.java
@@ -6,16 +6,14 @@ public class CaixaMercado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner teclado = new Scanner(System.in);
 		int condicao = 1;
-		double quantidadeItens = 0;
+		int quantidadeItens = 0;
+		double valorTotal = 0;
 		
 		String produtos = "código " + " produto " + "\n " + "1      0,50"
 		+ "\n " + "2      1,00" + "\n " + "3      4,00"  + "\n " + "5      7,00" + "\n " + "9      8,00";
-		
-		//System.out.println(produtos);
-		
+			
 		while (condicao != 0) {
 			 System.out.println(produtos);
 			 int escolhaUsuario = teclado.nextInt();
@@ -24,12 +22,49 @@ public class CaixaMercado {
 				 break;
 			 }
 			 
+			 else if (escolhaUsuario == 4 || escolhaUsuario > 5) {
+				 System.out.println("código inválido");
+				 System.out.println(" ");
+				 
+			 }
+			
 			 else if (escolhaUsuario == 1) {
+				 System.out.println("informe a quantidade de items que deseja: ");
+				 quantidadeItens = teclado.nextInt();
+				 valorTotal =  quantidadeItens * 0.50;
+				 
+			 }
+			 
+			 else if (escolhaUsuario == 2) {
+				 System.out.println("informe a quantidade de items que deseja: ");
+				 quantidadeItens = teclado.nextInt();
+				 valorTotal =  quantidadeItens * 1.00;
+				 
+			 }
+			 
+			 else if (escolhaUsuario == 3) {
+				 System.out.println("informe a quantidade de items que deseja: ");
+				 quantidadeItens = teclado.nextInt();
+				 valorTotal =  quantidadeItens * 4.00;
+				 
+			 }
+			 
+			 else if (escolhaUsuario == 5) {
+				 System.out.println("informe a quantidade de items que deseja: ");
+				 quantidadeItens = teclado.nextInt();
+				 valorTotal =  quantidadeItens * 7.00;
+				 
+			 }
+			 
+			 else if (escolhaUsuario == 9) {
+				 System.out.println("informe a quantidade de items que deseja: ");
+				 quantidadeItens = teclado.nextInt();
+				 valorTotal =  quantidadeItens * 8.00;
 				 
 			 }
 		}
 		
-		System.out.println("Laço parado");
+		System.out.println("Valor total dos itens " + valorTotal);
 		teclado.close();
 
 	}
