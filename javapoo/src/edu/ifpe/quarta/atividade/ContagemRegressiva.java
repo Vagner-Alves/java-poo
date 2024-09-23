0603a4eef606ea988e9ce2b6539c7c186f977be6
Vagner-Alves <vagneralves997@gmail.com>
2024-09-23 13:50:17 -0300

    laço determinado

---
+++ javapoo/src/edu/ifpe/quarta/atividade/ContagemRegressiva.java
@@ -0,0 +1,14 @@
+package edu.ifpe.quarta.atividade;
+
+public class ContagemRegressiva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int iniciar =0; iniciar < 10; iniciar ++) {
+			System.out.println(iniciar);
+		}
+
+	}
+
+}
