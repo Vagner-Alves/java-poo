2a999e55bea2c3637c6eac02a57f708e86eda5ca
Vagner-Alves <vagneralves997@gmail.com>
2024-09-23 13:54:00 -0300

    atualizando os exercicios

--- javapoo/src/edu/ifpe/quarta/atividade/ContagemRegressiva.java
+++ javapoo/src/edu/ifpe/quarta/atividade/ContagemRegressiva.java
@@ -5,9 +5,11 @@ public class ContagemRegressiva {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int iniciar =0; iniciar < 10; iniciar ++) {
+		for(int iniciar =20; iniciar >= 0; iniciar --) {
 			System.out.println(iniciar);
+			
 		}
+		System.out.println("Vagner Alves");
 
 	}
 
