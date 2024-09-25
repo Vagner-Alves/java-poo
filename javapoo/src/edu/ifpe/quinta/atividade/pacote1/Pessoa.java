0d02b2c9fd0d259f3bab84902be744b2496b563e
Vagner-Alves <vagneralves997@gmail.com>
2024-09-25 11:14:07 -0300

    exercicio semana 3

--- javapoo/src/edu/ifpe/quinta/atividade/pacote1/Pessoa.java
+++ javapoo/src/edu/ifpe/quinta/atividade/pacote1/Pessoa.java
@@ -5,6 +5,48 @@ public class Pessoa {
 	private int anoNascimento;
 	private double altura;
 	
+	private Pessoa() {}
+	
+	private Pessoa (String nome, int anoNascimento, double altura) {
+		this.nome = nome;
+		this.anoNascimento = anoNascimento;
+		this.altura = altura;
+	}
+	
+	private void imprimirDados(){
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Ano de nascimento: " + this.anoNascimento);
+		System.out.println("Altura: " + this.altura);
+	}
+	
+	private void calcularIdade() {
+		System.out.println("Nome:  " + this.nome);
+		System.out.println("Idade: " + (2024 - this.anoNascimento));
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
 	
 	
 
