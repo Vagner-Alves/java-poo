f52274acac2009925ea1ff58bcb0ff75d22994bb
Vagner Alves Ferreira da Silva <vafs@palmares.ifpe.edu.br>
2024-09-25 10:34:15 -0300

    pacotes e modificadores

---
+++ javapoo/src/edu/ifpe/quinta/atividade/Conta.java
@@ -0,0 +1,16 @@
+package edu.ifpe.quinta.atividade;
+
+public class Conta {
+	public String titular;
+	String numeroConta;
+	private double saldoConta;
+	
+	private void creditar() {}
+	
+	public void debitar() {}
+	
+	double consultarSaldo() {
+		return saldoConta;
+	}
+
+}
---
+++ javapoo/src/edu/ifpe/quinta/atividade/MeuPrograma.java
@@ -0,0 +1,12 @@
+package edu.ifpe.quinta.atividade;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta conta = new Conta(); // é possivel pois as classes estão no mesmo pacote
+		
+		System.out.println(conta);
+	}
+
+}
