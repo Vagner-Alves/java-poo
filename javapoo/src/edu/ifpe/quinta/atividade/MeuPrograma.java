0791ae4361755ed1ca08de9d2f762d2fbc403aa8
Vagner Alves Ferreira da Silva <vafs@palmares.ifpe.edu.br>
2024-09-25 10:41:44 -0300

    pacotes e modificadores

--- javapoo/src/edu/ifpe/quinta/atividade/MeuPrograma.java
+++ javapoo/src/edu/ifpe/quinta/atividade/MeuPrograma.java
@@ -7,6 +7,14 @@ public class MeuPrograma {
 		Conta conta = new Conta(); // é possivel pois as classes estão no mesmo pacote
 		
 		System.out.println(conta);
+	//	conta.creditar();                              // esse método não é visivel , pois está privado
+		double saldo1 = conta.consultarSaldo();
+	//	double saldo2 = conta.saldoConta;             // o campo saldoConta não é visivel, pois está privado.
+		
+		String nome = conta.titular;		         // este campo está público portanto é visivel
+		String numeroConta = conta.numeroConta;      // esse campo está default,portanto é visivel para classes dentro do mesmo pacote
+		
+		
 	}
 
 }
