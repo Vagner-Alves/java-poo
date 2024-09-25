ce051629e4b2b04b990387cb7e5002558038f6d6
Vagner-Alves <vagneralves997@gmail.com>
2024-09-25 10:49:57 -0300

    testando pacotes

---
+++ javapoo/src/edu/ifpe/pacote/teste/MeuPrograma.java
@@ -0,0 +1,23 @@
+package edu.ifpe.pacote.teste;
+
+import edu.ifpe.quinta.atividade.Conta;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		Conta conta = new Conta(); // é possivel pois as classes estão no mesmo pacote
+		
+		System.out.println(conta);
+	//	conta.creditar();                              // esse método não é visivel , pois está privado
+		double saldo1 = conta.consultarSaldo();
+	//	double saldo2 = conta.saldoConta;             // o campo saldoConta não é visivel, pois está privado.
+		
+		String nome = conta.titular;		         // este campo está público portanto é visivel
+		String numeroConta = conta.numeroConta;      // esse campo está default,portanto é visivel para classes dentro do mesmo pacote
+		
+
+	}
+
+}
