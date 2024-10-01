8d2943db8197d3d0adfb803ee51a4deeb0c9bd51
Vagner-Alves <vagneralves997@gmail.com>
2024-10-01 18:05:12 -0300

    atualizando os exercicios

---
+++ javapoo/src/edu/ifpe/sexta/atividade/ContaBancaria.java
@@ -0,0 +1,15 @@
+package edu.ifpe.sexta.atividade;
+
+public class ContaBancaria {
+	private String nomeCliente;
+	private int numConta;
+	private double saldo;
+	
+	public ContaBancaria(String nomeCliente, int numConta, double saldo) {
+		this.nomeCliente = nomeCliente;
+		this.numConta = numConta;
+		this.saldo = saldo;
+	}
+	
+	
+}
