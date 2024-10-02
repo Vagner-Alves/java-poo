f0cff5d997edc55d4f2ff90ea23d1eead8a6256a
Vagner-Alves <vagneralves997@gmail.com>
2024-10-02 10:06:05 -0300

    vetores e listas em python

---
+++ javapoo/src/edu/ifpe/setima/atividade/TesteVetor.java
@@ -0,0 +1,14 @@
+package edu.ifpe.setima.atividade;
+
+public class TesteVetor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] nomes = new String[4];
+		
+		
+		
+	}
+
+}
