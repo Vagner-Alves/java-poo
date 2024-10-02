21846566bf4710ccff1856eae0b08983602d0cae
Vagner-Alves <vagneralves997@gmail.com>
2024-10-02 10:20:27 -0300

    atividade 2 sobre vetores

---
+++ javapoo/src/edu/ifpe/setima/atividade/Pessoa.java
@@ -0,0 +1,58 @@
+package edu.ifpe.setima.atividade;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String dataNascimento;
+	private double altura;
+	
+	public Pessoa(String nome, String cpf, String dataNascimento, double altura) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.altura = altura;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public void imprimirDados(){
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Ano de nascimento: " + getDataNascimento());
+		System.out.println("Altura: " + this.altura);
+		System.out.println("CPF: " + getCpf());
+		
+	}
+	
+
+}
--- javapoo/src/edu/ifpe/setima/atividade/TesteVetor.java
+++ javapoo/src/edu/ifpe/setima/atividade/TesteVetor.java
@@ -5,9 +5,15 @@ public class TesteVetor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] nomes = new String[4];
+		String[] nomes = new String[3];
 		
+		nomes[0] = "João";
+		nomes[1] = "Maria";
+		nomes[2] = "José";
 		
+		for(String lista: nomes) {
+			System.out.println(lista);
+		}
 		
 	}
 
