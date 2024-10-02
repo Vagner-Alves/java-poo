c2c31b9c74723ae01ea673fbfeafc777503f4aff
Vagner-Alves <vagneralves997@gmail.com>
2024-10-02 10:44:07 -0300

    estudando vetores em java

--- javapoo/src/edu/ifpe/setima/atividade/TesteVetor.java
+++ javapoo/src/edu/ifpe/setima/atividade/TesteVetor.java
@@ -1,11 +1,15 @@
 package edu.ifpe.setima.atividade;
 
+import java.util.Scanner;
+
 public class TesteVetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		String[] nomes = new String[3];
+		Scanner teclado = new Scanner(System.in);
+		int condicao = 1;
 		
 		nomes[0] = "João";
 		nomes[1] = "Maria";
@@ -17,18 +21,27 @@ public class TesteVetor {
 		
 		System.out.println(" ");
 		
-		Pessoa[] listadePessoas = new Pessoa[2];
+		Pessoa[] listadePessoas = new Pessoa[3];
 		
-		Pessoa primeiraPessoa = new Pessoa("Vagner Alves","xxx.xxx.xxx-xx","05/01/1997", 1.65);
-		Pessoa segundaPessoa = new Pessoa("Larissa","xxx.xxx.xxx-yy","05/01/1996", 1.70);
+		Pessoa primeiraPessoa = new Pessoa();
+		Pessoa segundaPessoa = new Pessoa();
+		Pessoa terceiraPessoa = new Pessoa();
+		
 		
 		listadePessoas[0] = primeiraPessoa;
 		listadePessoas[1] = segundaPessoa;
 		
 		
 		for( Pessoa lista: listadePessoas) {
-			lista.imprimirDados();
+			System.out.println("insira as informações de uma Pessoa (nome, cpf, data de nascimento e altura");
+			String nome = teclado.nextLine();
+			final String cpf = teclado.nextLine();
+			String dataNascimento = teclado.nextLine();
+			double altura = teclado.nextDouble();
+			
+			
 		}
+		
 		
 	}
 
