ebb7a88e7cc59c6a3b3f87828d375cf7c8d4fb5f
Vagner-Alves <vagneralves997@gmail.com>
2024-10-02 11:13:40 -0300

    switch case

---
+++ javapoo/src/edu/ifpe/setima/atividade/LeituradoMes.java
@@ -0,0 +1,46 @@
+package edu.ifpe.setima.atividade;
+
+import java.util.Scanner;
+
+public class LeituradoMes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		int escolhadoUsuario = teclado.nextInt();
+		
+		switch(escolhadoUsuario) {
+		
+		case 1:
+			System.out.println("Janeiro");
+		case 2:
+			System.out.println("Janeiro");
+		case 3:
+			System.out.println("Janeiro");
+		case 4:
+			System.out.println("Janeiro");
+		case 5:
+			System.out.println("Janeiro");
+		case 6:
+			System.out.println("Janeiro");
+		case 7:
+			System.out.println("Janeiro");
+		case 8:
+			System.out.println("Janeiro");
+		case 9:
+			System.out.println("Janeiro");
+		case 10:
+			System.out.println("Janeiro");
+		case 11:
+			System.out.println("Janeiro");
+		case 12:
+			System.out.println("Janeiro");
+		
+		default:
+			System.out.println("Código inválido");
+		}
+
+	}
+
+}
