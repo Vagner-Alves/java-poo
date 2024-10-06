0409d6803c27724b54651fc7f18d46fe3625b69f
Vagner-Alves <vagneralves997@gmail.com>
2024-10-06 19:00:18 -0300

    atualizando os exercicios

--- javapoo/src/edu/ifpe/setima/atividade/ContatoMenu.java
+++ javapoo/src/edu/ifpe/setima/atividade/ContatoMenu.java
@@ -8,6 +8,10 @@ public class ContatoMenu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+			String menuOpcoes = "\n AGENDA DE CONTATOS " + "\n" + "1 adicionar" 
+			+ "\n" + "2 consultar" + "\n" +  "3 alterar" + "\n" + "4 excluir";
+			
+			System.out.println(menuOpcoes);
 	    }
 
 
