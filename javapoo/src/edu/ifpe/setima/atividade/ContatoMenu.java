246aa0a9f12a5bffca3fe8c9e6702eb16b55e228
Vagner-Alves <vagneralves997@gmail.com>
2024-10-06 10:58:13 -0300

    atualizando os exercicios

--- javapoo/src/edu/ifpe/setima/atividade/ContatoMenu.java
+++ javapoo/src/edu/ifpe/setima/atividade/ContatoMenu.java
@@ -1,13 +1,19 @@
 package edu.ifpe.setima.atividade;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ContatoMenu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		ArrayList<Contato> agenda = new ArrayList<Contato>();
 		
+		String menu = "Opção " + " Ação " + "\n " + "1  adicionar" + "\n " + "2  consultar" + "\n "
+		+ "3  alterar" + "\n " + "4  excluir contato";
+		
+		System.out.println(menu);
 		
 		
 	}
