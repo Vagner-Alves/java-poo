c93702ded6e259b3a05820d939ac9a085939643c
Vagner-Alves <vagneralves997@gmail.com>
2024-10-09 10:59:42 -0300

    classe pessoa e arraylist

--- javapoo/src/edu/ifpe/oitava/atividade/ListaPessoas.java
+++ javapoo/src/edu/ifpe/oitava/atividade/ListaPessoas.java
@@ -7,12 +7,26 @@ public class ListaPessoas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		ArrayList<Pessoa> listadePessoas = new ArrayList<Pessoa>();
 		Scanner entradaUsuario = new Scanner(System.in);
-		
-		
-		
+
+		System.out.println("informe o nome do da pessoa: ");
+		String nome = entradaUsuario.nextLine();
+		System.out.println("informe o cpf : ");
+		String cpf = entradaUsuario.nextLine();
+		System.out.println("informe a data de nascimento: ");
+		String dataNascimento = entradaUsuario.nextLine();
+		System.out.println("informe a altura: ");
+		double altura = entradaUsuario.nextDouble();
+
+		Pessoa pessoa = new Pessoa(nome, cpf,dataNascimento, altura);
+
+		String opcao = entradaUsuario.nextLine();
+
+
+
+
 	}
 
 }
