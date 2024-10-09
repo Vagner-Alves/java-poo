389c888a440b4d49240d9a999fb80bf33f0fe627
Vagner-Alves <vagneralves997@gmail.com>
2024-10-09 10:45:46 -0300

    classe pessoa e arraylist

---
+++ javapoo/src/edu/ifpe/oitava/atividade/ListaPessoas.java
@@ -0,0 +1,18 @@
+package edu.ifpe.oitava.atividade;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListaPessoas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Pessoa> listadePessoas = new ArrayList<Pessoa>();
+		Scanner entradaUsuario = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
