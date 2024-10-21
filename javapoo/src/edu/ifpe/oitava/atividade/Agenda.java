445aacbf6ad7aa883cce5a059af0e0d7f6bafeb7
Vagner-Alves <vagneralves997@gmail.com>
2024-10-21 10:23:07 -0300

    atualizando atividade

--- javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
+++ javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
@@ -2,5 +2,15 @@ package edu.ifpe.oitava.atividade;
 
 public class Agenda {
 	
-
+	private String nome;
+	private String telefone;
+	
+	public Agenda() {}
+	
+	public Agenda(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone  = telefone;
+	}
+	
+	
 }
