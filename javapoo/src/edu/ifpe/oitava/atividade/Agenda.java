dcef145eef1e8fce3ee9068c21444f88d36ee707
Vagner-Alves <vagneralves997@gmail.com>
2024-10-23 10:01:50 -0300

    agenda de contatos

--- javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
+++ javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
@@ -7,43 +7,48 @@ import edu.ifpe.setima.atividade.Contato;
 
 public class Agenda {
 	private ArrayList<Contato> contatos;
-	
-	   public void adicionarContato(String nome, String telefone) {
-	        Contato contato = new Contato(nome, telefone);
-	        contatos.add(contato);
-	        System.out.println("Contato " + nome + " adicionado!");
-	    }
 
-	    
-	    public Contato consultarContato(String nome) {
-	        for (Contato contato : contatos) {
-	            if (contato.getNome().equalsIgnoreCase(nome)) {
-	                return contato;
-	            }
-	        }
-	        return null;
-	    }
+	public Agenda() {
+		contatos = new ArrayList<>();
+	}
 
-	    
-	    public void alterarContato(String nome, String novoTelefone) {
-	        Contato contato = consultarContato(nome);
-	        if (contato != null) {
-	            contato.setTelefone(novoTelefone);
-	            System.out.println("Telefone de " + nome + " alterado!");
-	        } else {
-	            System.out.println("Contato " + nome + " não encontrado.");
-	        }
-	    }
 
-	  
-	    public void excluirContato(String nome) {
-	        Contato contato = consultarContato(nome);
-	        if (contato != null) {
-	            contatos.remove(contato);
-	            System.out.println("Contato " + nome + " excluído!");
-	        } else {
-	            System.out.println("Contato " + nome + " não encontrado.");
-	        }
-	    }
-		
+	public void adicionarContato(String nome, String telefone) {
+		Contato contato = new Contato(nome, telefone);
+		contatos.add(contato);
+		System.out.println("Contato " + nome + " adicionado!");
+	}
+
+
+	public Contato consultarContato(String nome) {
+		for (Contato contato : contatos) {
+			if (contato.getNome().equalsIgnoreCase(nome)) {
+				return contato;
+			}
+		}
+		return null;
+	}
+
+
+	public void alterarContato(String nome, String novoTelefone) {
+		Contato contato = consultarContato(nome);
+		if (contato != null) {
+			contato.setTelefone(novoTelefone);
+			System.out.println("Telefone de " + nome + " alterado!");
+		} else {
+			System.out.println("Contato " + nome + " não encontrado.");
+		}
+	}
+
+
+	public void excluirContato(String nome) {
+		Contato contato = consultarContato(nome);
+		if (contato != null) {
+			contatos.remove(contato);
+			System.out.println("Contato " + nome + " excluído!");
+		} else {
+			System.out.println("Contato " + nome + " não encontrado.");
+		}
+	}
+
 }
--- javapoo/src/edu/ifpe/oitava/atividade/AgendaMenu.java
+++ javapoo/src/edu/ifpe/oitava/atividade/AgendaMenu.java
@@ -2,6 +2,8 @@ package edu.ifpe.oitava.atividade;
 
 import java.util.Scanner;
 
+import edu.ifpe.setima.atividade.Contato;
+
 public class AgendaMenu {
 
 	public static void main(String[] args) {
@@ -11,6 +13,53 @@ public class AgendaMenu {
 		
 		String menuOpcoes = "\n AGENDA DE CONTATOS " + "\n" + "1 adicionar contato" 
 				+ "\n" + "2 consultar" + "\n" +  "3 alterar contato" + "\n" + "4 excluir contato" + "\n" + "5 sair";
+		
+		while(true) {
+			System.out.println(menuOpcoes);
+
+			System.out.println("Escolha uma opção: ");
+			int opcao = teclado.nextInt();
+			teclado.nextLine();
+
+			switch(opcao) {
+
+			case 1:
+				System.out.println("informe o nome: ");
+				String nome = teclado.nextLine();
+				System.out.println("informe o número de telefone: ");
+				String telefone = teclado.nextLine();
+				agendaContatos.adicionarContato(nome, telefone);
+				break;
+
+			case 2:
+				System.out.println("informe o nome do contato que deseja pesquisar: ");
+				nome  = teclado.nextLine();
+				Contato contato = agendaContatos.consultarContato(nome);
+				System.out.println("Nome: " + contato.getNome() + " telefone " + contato.getTelefone() );
+
+			case 3:
+				System.out.println("informe o nome do contato que deseja alterar: ");
+				nome  = teclado.nextLine();
+				System.out.println("informe o novo número de telefone: ");
+				String novoNumero = teclado.nextLine();
+				agendaContatos.alterarContato(nome, novoNumero);
+				break;
+			
+			case 4:
+				System.out.println("informe o nome do contato que deseja excluir: ");
+				nome  = teclado.nextLine();
+				agendaContatos.excluirContato(nome);
+				break;
+			
+			case 5:
+				System.out.println("saindo da lista de contatos: ");
+				teclado.close();
+				return;
+				
+			default:
+				System.out.println("não existe a opção de número: " + opcao);
+			}
+		}
 
 	}
 
