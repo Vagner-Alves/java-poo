cdca1dbe8424d1c518531ddb9856d897b9f0fcf9
Vagner-Alves <vagneralves997@gmail.com>
2024-10-23 11:20:12 -0300

    conta bancaria

--- javapoo/src/edu/ifpe/nona/atividade/conta/Poupanca.java
+++ javapoo/src/edu/ifpe/nona/atividade/conta/Poupanca.java
@@ -1,5 +1,20 @@
 package edu.ifpe.nona.atividade.conta;
 
-public class Poupanca {
+import edu.ifpe.nona.atividade.cliente.Cliente;
 
+public class Poupanca extends Conta{
+	private double juros;
+	
+	public Poupanca(String numero, double saldo, Cliente cliente, double juros) {
+		super(numero, saldo, cliente);
+		this.juros = juros;
+	}
+	
+	public double getJuros() {
+		return juros;
+	}
+	
+	public void setJuros(double juros) {
+		this.juros = juros;
+	}
 }
--- javapoo/src/edu/ifpe/nona/atividade/principal/MenuCaixaEletronico.java
+++ javapoo/src/edu/ifpe/nona/atividade/principal/MenuCaixaEletronico.java
@@ -3,6 +3,7 @@ package edu.ifpe.nona.atividade.principal;
 public class MenuCaixaEletronico {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 	}
