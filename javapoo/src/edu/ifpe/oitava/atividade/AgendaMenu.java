36408e14042b0982ec697dd1f70dcb2ab18a4aae
Vagner-Alves <vagneralves997@gmail.com>
2024-10-23 09:54:11 -0300

    agenda de contatos

---
+++ javapoo/src/edu/ifpe/oitava/atividade/AgendaMenu.java
@@ -0,0 +1,17 @@
+package edu.ifpe.oitava.atividade;
+
+import java.util.Scanner;
+
+public class AgendaMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Agenda agendaContatos = new Agenda();
+		Scanner teclado = new Scanner(System.in);
+		
+		String menuOpcoes = "\n AGENDA DE CONTATOS " + "\n" + "1 adicionar contato" 
+				+ "\n" + "2 consultar" + "\n" +  "3 alterar contato" + "\n" + "4 excluir contato" + "\n" + "5 sair";
+
+	}
+
+}
