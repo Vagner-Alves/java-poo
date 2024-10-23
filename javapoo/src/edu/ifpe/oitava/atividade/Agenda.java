903ff9d65bfff78da17ad19ae92ef7c254c7cf82
Vagner-Alves <vagneralves997@gmail.com>
2024-10-23 09:50:10 -0300

    editando lista de contatos

--- javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
+++ javapoo/src/edu/ifpe/oitava/atividade/Agenda.java
@@ -2,8 +2,48 @@ package edu.ifpe.oitava.atividade;
 
 import java.util.ArrayList;
 
+
+import edu.ifpe.setima.atividade.Contato;
+
 public class Agenda {
-	private ArrayList<Pessoa> listaPessoas;
+	private ArrayList<Contato> contatos;
 	
-	
+	   public void adicionarContato(String nome, String telefone) {
+	        Contato contato = new Contato(nome, telefone);
+	        contatos.add(contato);
+	        System.out.println("Contato " + nome + " adicionado!");
+	    }
+
+	    
+	    public Contato consultarContato(String nome) {
+	        for (Contato contato : contatos) {
+	            if (contato.getNome().equalsIgnoreCase(nome)) {
+	                return contato;
+	            }
+	        }
+	        return null;
+	    }
+
+	    
+	    public void alterarContato(String nome, String novoTelefone) {
+	        Contato contato = consultarContato(nome);
+	        if (contato != null) {
+	            contato.setTelefone(novoTelefone);
+	            System.out.println("Telefone de " + nome + " alterado!");
+	        } else {
+	            System.out.println("Contato " + nome + " não encontrado.");
+	        }
+	    }
+
+	  
+	    public void excluirContato(String nome) {
+	        Contato contato = consultarContato(nome);
+	        if (contato != null) {
+	            contatos.remove(contato);
+	            System.out.println("Contato " + nome + " excluído!");
+	        } else {
+	            System.out.println("Contato " + nome + " não encontrado.");
+	        }
+	    }
+		
 }
