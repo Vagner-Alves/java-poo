4865c3588261f9feaee7c62fcf1f745c08d1e768
Vagner-Alves <vagneralves997@gmail.com>
2024-11-02 19:04:07 -0300

    for each em array lists

--- javapoo/src/edu/ifpe/estudos/arrays/ArraylistLoop.java
+++ javapoo/src/edu/ifpe/estudos/arrays/ArraylistLoop.java
@@ -2,10 +2,27 @@ package edu.ifpe.estudos.arrays;
 import java.util.ArrayList;
 
 public class ArraylistLoop {
-	
-	public static void main(String[] args) {
-		
-		
-	}
 
+	public static void main(String[] args) {
+
+		ArrayList<String> comprasMercado = new ArrayList<String>();
+
+		comprasMercado.add("Banana");
+		comprasMercado.add("Aveia em flocos");
+		comprasMercado.add("Granola");
+		comprasMercado.add("Maçã");
+		comprasMercado.add("Barrinha de Cereal");
+
+		for(int items=0; items < comprasMercado.size(); items++) {
+			System.out.println(comprasMercado.get(items));
+		}
+		
+		System.out.println( );
+		
+		// utilizando FOR EACH
+		for(String items: comprasMercado) {
+			System.out.println(items);
+		}
+
+	}
 }
