e390260022cfda1677e29caf8d1d1d0109029d38
Vagner-Alves <vagneralves997@gmail.com>
2024-11-02 18:19:10 -0300

    metodos da classe array

--- javapoo/src/edu/ifpe/estudos/arrays/Arraylists.java
+++ javapoo/src/edu/ifpe/estudos/arrays/Arraylists.java
@@ -12,6 +12,28 @@ public class Arraylists {
 		cars.add("porsche - roxo");
 		
 		System.out.println(cars);
+		
+		// adicionar items escolhendo a posição
+		cars.add(0, "aston martin");
+		System.out.println(cars);
+		
+		// acessar um item no ArrayList
+		System.out.println(cars.get(1));
+		
+		// modificando um elemento, utilizando indice
+		cars.set(0, "aston martin - verde e amarelo");
+		System.out.println(cars);
+		
+		// removendo um item 
+		cars.remove(1);
+		System.out.println(cars);
+		
+		// removendo todos os items ( limpando a lista)
+		cars.clear();
+		System.out.println(cars);
+		
+		// ver o tamanho do ArrayList
+		System.out.println(cars.size());
 	}
 
 }
