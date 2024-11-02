986cc143614f7dc9a61a8981dc2154a43920e36a
Vagner-Alves <vagneralves997@gmail.com>
2024-11-02 18:06:38 -0300

    estudo de arrayList

---
+++ javapoo/src/edu/ifpe/estudos/arrays/Arraylists.java
@@ -0,0 +1,17 @@
+package edu.ifpe.estudos.arrays;
+import java.util.ArrayList;
+public class Arraylists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("fusca");
+		cars.add("BMW");
+		cars.add("brasilia amarela");
+		cars.add("porsche - roxo");
+		
+		System.out.println(cars);
+	}
+
+}
