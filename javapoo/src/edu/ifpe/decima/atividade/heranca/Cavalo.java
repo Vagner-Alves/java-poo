5a5729925d06b452b946b7f1334b9223dbea7f06
Vagner-Alves <vagneralves997@gmail.com>
2024-11-04 12:52:44 -0300

    atividade herança

--- javapoo/src/edu/ifpe/decima/atividade/heranca/Cavalo.java
+++ javapoo/src/edu/ifpe/decima/atividade/heranca/Cavalo.java
@@ -1,5 +1,30 @@
 package edu.ifpe.decima.atividade.heranca;
 
-public class Cavalo {
+public class Cavalo extends Animal {
+	private String raca;
 
+	public Cavalo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cavalo(float tamanho, String cor, String raca) {
+		super(tamanho, cor);
+		this.raca = raca;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void fugir() {
+		System.out.println("O cavalo está fugindo!");
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	
+	
 }
--- javapoo/src/edu/ifpe/decima/atividade/heranca/Leao.java
+++ javapoo/src/edu/ifpe/decima/atividade/heranca/Leao.java
@@ -1,5 +1,27 @@
 package edu.ifpe.decima.atividade.heranca;
 
-public class Leao {
-
+public class Leao extends Animal {
+	private boolean juba;
+	
+	public Leao() {
+		super();
+	}
+	
+	public Leao(float tamanho, String cor, boolean juba) {
+		super(tamanho, cor);
+		this.juba = juba;
+	}
+	
+	public void cacar() {
+		System.out.println("O leão começou a caçada!");
+	}
+	
+	public boolean getJuba() {
+		return juba;
+	}
+	
+	
+	public void setJuba(boolean juba) {
+		this.juba = juba;
+	}
 }
