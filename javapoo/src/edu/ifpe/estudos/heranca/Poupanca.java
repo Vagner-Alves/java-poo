b84b87053af793d434796e8523f688204c084dd1
Vagner-Alves <vagneralves997@gmail.com>
2024-11-03 23:03:31 -0300

    pequenas mudanças no codigo

--- javapoo/src/edu/ifpe/estudos/heranca/Poupanca.java
+++ javapoo/src/edu/ifpe/estudos/heranca/Poupanca.java
@@ -1,6 +1,22 @@
 package edu.ifpe.estudos.heranca;
 
 public class Poupanca {
+	private String numero;
+	private double saldo;
+	private double juros;
 	
+	public Poupanca() {}
+	
+	public Poupanca(String n, double s, double j) {
+		numero = n; saldo = s; juros=j;
+	}
+	
+	void creditar(double valor) {
+		saldo = saldo + valor;
+	}
+	
+	void renderJuros() {
+		creditar(saldo * juros);
+	}
 
 }
