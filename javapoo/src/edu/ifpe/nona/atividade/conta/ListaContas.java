7d6e585dbc484375a003dc6ad018595980ef0bd5
Vagner-Alves <vagneralves997@gmail.com>
2024-11-05 23:49:24 -0300

    projeto oo

--- javapoo/src/edu/ifpe/nona/atividade/conta/ListaContas.java
+++ javapoo/src/edu/ifpe/nona/atividade/conta/ListaContas.java
@@ -2,19 +2,45 @@ package edu.ifpe.nona.atividade.conta;
 
 import java.util.ArrayList;
 
+import edu.ifpe.nona.atividade.cliente.Cliente;
+
 public class ListaContas {
 	private ArrayList<Conta> listaContas;
+	private ArrayList<Cliente> listaClientes;
 	
 	public ListaContas() {
 		listaContas = new ArrayList<>();
+		listaClientes = new ArrayList<>();
 	}
 	
-	public void adicionarConta() {}
+	public void adicionarCliente(String nome, String cpf) {
+		Cliente cliente = new Cliente(nome, cpf);
+		listaClientes.add(cliente);
+	}
 	
-	public void consultarConta() {}
+	public void adicionarConta(String numero, double saldo, Cliente cliente) {
+		Conta conta = new Conta(numero, saldo, cliente);
+		listaContas.add(conta);
+		
+		
+	}
 	
-	public void fazerDeposito() {}
+	public Conta consultarConta(String nome) {
+		for(Conta items: listaContas) {
+			if(items.cliente.getNome().equalsIgnoreCase(nome)) {
+				return items;
+			}
+		} return null;
+	}
 	
-	public void fazerTransferencia() {}
+	public void fazerDeposito(String nome) {
+		Conta conta = consultarConta(nome);
+		
+		
+	}
+	
+	public void fazerTransferencia(String nome) {
+		Conta conta = consultarConta(nome);
+	}
 
 }
