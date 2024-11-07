c0d580ebdc1e65f8fe506a666f07c41f938ece44
Vagner-Alves <vagneralves997@gmail.com>
2024-11-07 01:21:46 -0300

    atualizando os exercicios

--- javapoo/src/edu/ifpe/nona/atividade/principal/Menu.java
+++ javapoo/src/edu/ifpe/nona/atividade/principal/Menu.java
@@ -2,6 +2,8 @@ package edu.ifpe.nona.atividade.principal;
 
 import java.util.Scanner;
 
+import edu.ifpe.nona.atividade.cliente.Cliente;
+import edu.ifpe.nona.atividade.conta.Conta;
 import edu.ifpe.nona.atividade.conta.ListaContas;
 import edu.ifpe.nona.atividade.conta.Poupanca;
 
@@ -33,6 +35,7 @@ public class Menu {
 				String cpf = teclado.nextLine();
 				contas.adicionarCliente(nome, cpf);
 				//contas.consultarConta("vagner alves");
+				break;
 				
 			case 2:
 				System.out.println("informe o número da conta: ");
@@ -40,15 +43,22 @@ public class Menu {
 				System.out.println("informe o saldo da conta: ");
 				double saldoConta = teclado.nextDouble();
 				contas.adicionarConta(numeroConta, saldoConta, null);
-				
+				break;
 			
 			case 3:
-				System.out.println("informe o número da conta (poupanca): ");
-				String numeroContaPoupanca = teclado.nextLine();
-				System.out.println("informe o saldo da conta (poupanca): ");
-				double saldoContaPoupanca = teclado.nextDouble();
-				contas.adicionarContaPoupanca(numeroContaPoupanca, saldoContaPoupanca, null,0);
-				
+				System.out.println("Informe o número da conta (poupanca): "); 
+				String numeroContaPoupanca = teclado.nextLine(); 
+				System.out.println("Informe o saldo da conta (poupanca): ");
+				double saldoContaPoupanca = teclado.nextDouble(); 
+				teclado.nextLine(); 
+				System.out.println("Informe o nome do cliente: ");
+				String nomeClientePoupanca = teclado.nextLine();
+				Cliente clientePoupanca = contas.consultarConta(nomeClientePoupanca).getCliente();
+				System.out.println("Taxa de juros: "); double juros = teclado.nextDouble(); 
+				teclado.nextLine();  
+				contas.adicionarContaPoupanca(numeroContaPoupanca, saldoContaPoupanca, clientePoupanca, juros); 
+				System.out.println("Poupança criada com sucesso!");
+				break;
 			
 			case 4:
 				System.out.println("informe o valor a ser depositado: ");
@@ -56,12 +66,14 @@ public class Menu {
 				System.out.println("informa o titular da conta (nome): ");
 				String nomeTitular = teclado.nextLine();
 				contas.fazerDeposito(nomeTitular, valor);
+				break;
 				
 			
 			case 5:
 				System.out.println("informe o titular da conta (nome): ");
 				String consulta = teclado.nextLine();
 				contas.consultarConta(consulta);
+				break;
 			
 			case 6:
 				System.out.println("informe o valor a ser transferido: ");
@@ -71,16 +83,23 @@ public class Menu {
 				System.out.println("informe a conta de destino: ");
 				String contaDestino = teclado.nextLine();
 				contas.fazerTransferencia(contaOrigem, contaDestino, valorTransferencia);
+				break;
 			
 			case 7:
-				System.out.println("informe o titular da conta ( poupanca ): ");
-				String contaJuros = teclado.nextLine();
+				System.out.println("Informe o nome do cliente: "); 
+				String nomeJuros = teclado.nextLine(); 
+				Conta contaJuros = contas.consultarConta(nomeJuros); 
+				if (contaJuros instanceof Poupanca) { ((Poupanca) contaJuros).renderJuros(5); 
+				System.out.println("Juros rendidos com sucesso!"); }
+				else { System.out.println("Esta conta não é uma poupança."); }
 				
 			case 8:
 				System.out.println("obrigado por usar o sistema: ");
+				break;
 			
 			default:
 				System.out.println("opção invalida , tente novamente.");
+				break;
 				
 				
 			}
