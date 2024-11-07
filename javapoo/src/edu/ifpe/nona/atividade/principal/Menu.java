72eb6a40e87e153c6b40779f327e1bbb526be521
Vagner-Alves <vagneralves997@gmail.com>
2024-11-06 23:50:59 -0300

    atualizando codigo

--- javapoo/src/edu/ifpe/nona/atividade/principal/Menu.java
+++ javapoo/src/edu/ifpe/nona/atividade/principal/Menu.java
@@ -1,11 +1,36 @@
 package edu.ifpe.nona.atividade.principal;
 
+import java.util.Scanner;
+
+import edu.ifpe.nona.atividade.conta.ListaContas;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ListaContas contas = new ListaContas();
+		Scanner teclado = new Scanner(System.in);
 		
-
+		String menuOpcoes = "\n MENU DO BANCO " + "\n" + "1 Criar Cliente" 
+				+ "\n" + "2 Criar Conta" 
+				+ "\n" +  "3 Criar Poupança" + 
+				"\n" + "4 Depositar" + "\n" + "5 Consultar Saldo"
+				+ "\n" + "6 Transferir Dinheiro" + "\n" + "7 Render Juros" + "\n" + "8 Sair";
+		
+		while(true) {
+			System.out.println(menuOpcoes);
+			System.out.println("Escolha uma opção: ");
+			int opcao = teclado.nextInt();
+			teclado.nextLine();
+			
+			switch(opcao) {
+				
+			case 1:
+				
+				
+			}
+		}
+		
 	}
 
 }
